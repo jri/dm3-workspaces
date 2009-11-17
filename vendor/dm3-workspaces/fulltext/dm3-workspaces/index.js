6158bef4f8b5939a8ffa5b11a1fbf0d81fad6bad
Jörg Richter <jri@deepamehta.de>
2009-11-17 17:44:30 +0100

    Adapted again to current version of DeepaMehta 3.

--- vendor/dm3-workspaces/fulltext/dm3-workspaces/index.js
+++ vendor/dm3-workspaces/fulltext/dm3-workspaces/index.js
@@ -7,7 +7,23 @@ function(doc) {
     var ret = new Document();
 
     ret.add(doc.topic_type, {store: "yes", index: "not_analyzed"})
-    ret.add(doc.fields[0].content, {store: "yes"});
+    ret.add(get_field("Name").content, {store: "yes"});
 
     return ret;
+
+
+
+    /***************/
+    /*** Helpers ***/
+    /***************/
+
+
+
+    function get_field(field_id) {
+        for (var i = 0, field; field = doc.fields[i]; i++) {
+            if (field.id == field_id) {
+                return field
+            }
+        }
+    }
 }
