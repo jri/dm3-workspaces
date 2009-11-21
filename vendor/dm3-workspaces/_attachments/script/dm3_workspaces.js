3c2f07768eaad7196766be50cd03a7001afc0d08
Jörg Richter <jri@deepamehta.de>
2009-11-21 16:45:33 +0100

    1) Bug fix: when creating a topic a workspace is assigned by default. 2) Internal change: code simplied and adapted to current version of UI Helper API.

--- vendor/dm3-workspaces/_attachments/script/dm3_workspaces.js
+++ vendor/dm3-workspaces/_attachments/script/dm3_workspaces.js
@@ -1,48 +1,51 @@
 function dm3_workspaces() {
+
     css_stylesheet("vendor/dm3-workspaces/style/dm3-workspaces.css")
     topic_type_icons["Workspace"] = create_image("vendor/dm3-workspaces/images/star.png")
-}
 
-dm3_workspaces.prototype = {
 
-    init: function() {
 
-        var workspaces = this.get_all_workspaces()
-        create_default_workspace(this)
-        create_workspace_selector(this)
-        create_workspace_dialog(this)
+    /**************************************************************************************************/
+    /**************************************** Overriding Hooks ****************************************/
+    /**************************************************************************************************/
 
-        function create_default_workspace(module) {
+
+
+    this.init = function() {
+
+        var workspaces = get_all_workspaces()
+        create_default_workspace()
+        create_workspace_selector()
+        create_workspace_dialog()
+
+        function create_default_workspace() {
             if (workspaces.rows.length == 0) {
-                module.create_workspace("Default")
-                workspaces = module.get_all_workspaces()
+                create_workspace("Default")
+                workspaces = get_all_workspaces()
             }
         }
 
-        function create_workspace_selector(module) {
-            // update model
-            current_workspace_id = workspaces.rows[0].id
-            // update GUI
+        function create_workspace_selector() {
             var select = $("<div>").attr("id", "workspace_select")
             var workspace_selector = $("<div>").attr("id", "workspace_form").text("Workspace ").append(select)
             $("#upper_toolbar").prepend(workspace_selector)
-            ui.menu("workspace_select", module.workspace_selected, [])
-            module.update_workspace_selector(workspaces)
+            ui.menu("workspace_select", workspace_selected)
+            update_workspace_selector(workspaces)
         }
 
-        function create_workspace_dialog(module) {
+        function create_workspace_dialog() {
             var workspace_dialog = $("<div>").attr("id", "workspace_dialog")
             var input = $("<input>").attr({id: "workspace_name", size: 30})
             workspace_dialog.append("Name:")
-            workspace_dialog.append($("<form>").submit(module.do_create_workspace).append(input))
+            workspace_dialog.append($("<form>").submit(do_create_workspace).append(input))
             $("body").append(workspace_dialog)
             $("#workspace_dialog").dialog({modal: true, autoOpen: false, draggable: false, resizable: false, width: 350,
-                title: "New Workspace", buttons: {"OK": module.do_create_workspace}})
+                title: "New Workspace", buttons: {"OK": do_create_workspace}})
         }
-    },
+    }
 
     // Note: we use the pre_create hook to let the "Workspaces" field be saved also if the user cancels the initial editing.
-    pre_create: function(doc) {
+    this.pre_create = function(doc) {
         if (doc.type == "Topic") {
             doc.fields.push({
                 id: "Workspaces",
@@ -57,16 +60,16 @@ dm3_workspaces.prototype = {
         } else {
             // TODO: handle relations too
         }
-    },
+    }
 
     // Note: we must use the post_create hook to create the relation because at pre_create the document has no ID yet.
-    post_create: function(doc) {
+    this.post_create = function(doc) {
         // Note 1: we do not relate search results to a workspace. Otherwise the search result would appear
         // as relation when displaying the workspace. That's because an "Auxiliray" relation is not be
         // created if there is another relation already.
         // Note 2: we do not relate workspaces to a workspace. This would be contra-intuitive.
         if (doc.type == "Topic" && doc.topic_type != "Search Result" && doc.topic_type != "Workspace") {
-            var workspace_id = $("#workspace_select option:selected").attr("value")
+            var workspace_id = ui.menu_item("workspace_select").value
             // Note: workspace_id is undefined in case the doc is the (just created) default workspace itself.
             if (workspace_id) {
                 create_relation(doc._id, workspace_id)
@@ -74,7 +77,7 @@ dm3_workspaces.prototype = {
         } else {
             // TODO: assign relations to a workspace
         }
-    },
+    }
 
 
 
@@ -84,11 +87,11 @@ dm3_workspaces.prototype = {
 
 
 
-    get_all_workspaces: function() {
+    function get_all_workspaces() {
         return db.view("deepamehta3/by_type", {key: "Workspace"})
-    },
+    }
 
-    create_workspace: function(name) {
+    function create_workspace(name) {
         var fields = [
             {id: "Name",        model: {type: "text"}, view: {editor: "single line"}, content: name},
             {id: "Description", model: {type: "text"}, view: {editor: "multi line"},  content: ""}
@@ -96,34 +99,32 @@ dm3_workspaces.prototype = {
         var workspace = create_raw_topic("Workspace", fields, {}, "PlainDocument")
         save_document(workspace)
         return workspace
-    },
+    }
 
-    workspace_selected: function(menu_item) {
+    function workspace_selected(menu_item) {
         var value = menu_item.value
         log("Workspace selected: " + value)
         if (value == "_new") {
-            dm3_workspaces.prototype.new_workspace()
-        } else {
-            current_workspace_id = value
+            new_workspace()
         }
-    },
+    }
 
-    new_workspace: function() {
+    function new_workspace() {
         $("#workspace_dialog").dialog("open")
-    },
+    }
 
-    do_create_workspace: function() {
+    function do_create_workspace() {
         $("#workspace_dialog").dialog("close")
         var name = $("#workspace_name").val()
-        current_workspace_id = dm3_workspaces.prototype.create_workspace(name)._id
-        dm3_workspaces.prototype.update_workspace_selector()
-        dm3_workspaces.prototype.update_workspace_selection()
+        var workspace_id = create_workspace(name)._id
+        update_workspace_selector()
+        select_workspace(workspace_id)
         return false
-    },
+    }
 
-    update_workspace_selector: function(workspaces) {
+    function update_workspace_selector(workspaces) {
         if (!workspaces) {
-            workspaces = this.get_all_workspaces()
+            workspaces = get_all_workspaces()
         }
         ui.empty_menu("workspace_select")
         for (var i = 0, row; row = workspaces.rows[i]; i++) {
@@ -131,9 +132,9 @@ dm3_workspaces.prototype = {
         }
         ui.add_menu_separator("workspace_select")
         ui.add_menu_item("workspace_select", {label: "New Workspace...", value: "_new", is_trigger: true})
-    },
+    }
 
-    update_workspace_selection: function() {
-        ui.select_menu_item("workspace_select", current_workspace_id)
+    function select_workspace(workspace_id) {
+        ui.select_menu_item("workspace_select", workspace_id)
     }
 }
